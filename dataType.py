cc31c13eb4ece4ba4b69cd4780072e47a34e0c96
Umesh Rao <umeshyadav7988@gmail.com>
2025-03-06 22:27:59 +0530

    Data Types

---
+++ dataType.py
@@ -0,0 +1,38 @@
+# String
+
+# username = "Umeshrao"
+# print(len(username))
+# print(username[0])
+# # username[0]= "A"// error string are immutable
+# print(username[-1])
+# print(username[1:3])
+# print(dir(username))
+
+
+# List
+# mylist = [12,"Umeshrao",14.5]
+# print(mylist)
+# print(len(mylist))
+# print(mylist[0])
+# print(mylist[-1])
+# print(mylist[1:4])
+
+
+# Dictionaries 
+# myD = {'one': 'Lemon', 'two' : 'ginger','three':'comic'}
+# print(myD)
+# print(myD['one'])
+# print(myD['two'])
+# # print(myD['twoo']) # Key Error 
+
+
+#Tuples
+myTup = (1,2,3)
+
+print(myTup)
+print(myTup[0])
+len(myTup)
+print(myTup[len(myTup)-1])
+
+
+
